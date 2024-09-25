23b852b1410376896c911c319bc96754afb8e4dd
Anil Nair <anilcoll90@gmail.com>
2024-09-25 22:23:13 +0530

    Add project files.

---
+++ Powershell-Host/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Management.Automation;
+
+namespace Powershell_Host
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            PowerShell.Create().AddCommand("Copy-Item")
+                               .AddArgument(@"G:\examples\.gitignore")
+                               .AddParameter("-Destination", @"G:\")
+                               .Invoke();
+            Console.ReadLine();
+        }
+    }
+}
